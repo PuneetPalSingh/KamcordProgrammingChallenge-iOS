eb4437f9122ee9987c41d52943bbc83feb9b122b
PuneetPalSingh <puneetpalsingh5@gmail.com>
2016-03-18 17:15:48 -0700

    Initial Commit

---
+++ KamcordProgrammingChallenge-iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KamcordProgrammingChallenge-iOS
+//
+//  Created by Puneet Pal Singh on 3/18/16.
+//  Copyright © 2016 Puneet Pal Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ KamcordProgrammingChallenge-iOSTests/KamcordProgrammingChallenge_iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  KamcordProgrammingChallenge_iOSTests.m
+//  KamcordProgrammingChallenge-iOSTests
+//
+//  Created by Puneet Pal Singh on 3/18/16.
+//  Copyright © 2016 Puneet Pal Singh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KamcordProgrammingChallenge_iOSTests : XCTestCase
+
+@end
+
+@implementation KamcordProgrammingChallenge_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
